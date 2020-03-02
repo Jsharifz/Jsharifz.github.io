0ca93e39e2ad23e2e8da78e4efc447291ecb99f9
Javad Sharifzadeh-Najafi <59621765+Jsharifz@users.noreply.github.com>
2020-03-02 12:42:10 -0700

    minor changes

--- script/app.js
+++ script/app.js
@@ -1,12 +1,14 @@
-myID = document.getElementById("nav")
+myID = document.getElementById('nav')
+
+myID.className = 'nav-hide'
 
 var navScroll = function() {
-    var y = window.scrollY
-    if (y >= 350) {
-        myID.className = "nav-show"
-    } else {
-        myID.className = "nav-hide"
-    }
+  var y = window.scrollY
+  if (y >= 350) {
+    myID.className = 'nav-show'
+  } else {
+    myID.className = 'nav-hide'
+  }
 }
 
-window.addEventListener("scroll", navScroll)
+window.addEventListener('scroll', navScroll)
